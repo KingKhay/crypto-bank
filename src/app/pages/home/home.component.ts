b82c4f1efa49c2fe5138237aa6588b424fa2edc7
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-06-26 14:37:11 +0000

    refactor checkmark

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CryptoService} from "../../services/crypto.service";
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  constructor(private cryptoService: CryptoService) {
+  }
+
+
+  fetchData(){
+    this.cryptoService.getData().subscribe(
+      response => {
+        console.log(response)
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
 
   getDate(){
     return new Date().toLocaleDateString("en-GB");
